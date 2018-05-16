604d88068ca46be1d0025e6f6ebd42004d14f8c7
ChristianCamiloTabordaCampino1298 <christian.taborda@correounivalle.edu.co>
2018-05-16 17:46:00 -0500

    Create handler.h

---
+++ handler.h
@@ -0,0 +1,9 @@
+#ifndef HANDLER
+#define HANDLER
+
+//Cabeceras de los métodos implementados:
+void ejecutarPipe(char ** args1, char ** args2, int direccionar, char * archivo);
+int existe(char * archivo);
+int ejecutar(char * linea, char * archivo, int direccionar);
+
+#endif
